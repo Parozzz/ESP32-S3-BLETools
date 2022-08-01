a890968d6fd4bb4eef0da7b699812d26fdc1feab
Parozzz <paronetto.giacomo@gmail.com>
2022-08-01 19:00:09 +0200

    It WORKS!

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,9 @@
 #include <USB.h>
 #include <USBHIDKeyboard.h>
 #include <USBCDC.h>
-#include <WiFi.h>
 #include <NimBLEDevice.h>
+
+#include <HIDDescriptor.h>
 
 #define APPEARANCE_GENERIC 0x03C0
 #define APPEARANCE_KEYBOARD 0x03C1
@@ -83,6 +84,7 @@ USBHIDKeyboard Keyboard;
 
 static void usbEventCallback(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
 {
+
   if (event_base == ARDUINO_USB_EVENTS)
   {
     arduino_usb_event_data_t *data = (arduino_usb_event_data_t *)event_data;
@@ -141,101 +143,39 @@ static void usbEventCallback(void *arg, esp_event_base_t event_base, int32_t eve
   }
 }
 
-/*
-    b"\x05\x01"  # Usage Page (Generic Desktop Ctrls)
-    b"\x09\x06"  # Usage (Keyboard)
-    b"\xA1\x01"  # Collection (Application)
-    b"\x85\x01"  #   Report ID (1)
-    b"\x05\x07"  #   Usage Page (Kbrd/Keypad)
-    b"\x19\xE0"  #   Usage Minimum (\xE0)
-    b"\x29\xE7"  #   Usage Maximum (\xE7)
-    b"\x15\x00"  #   Logical Minimum (0)
-    b"\x25\x01"  #   Logical Maximum (1)
-    b"\x75\x01"  #   Report Size (1)
-    b"\x95\x08"  #   Report Count (8)
-    b"\x81\x02"  #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    b"\x81\x01"  #   Input (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    b"\x19\x00"  #   Usage Minimum (\x00)
-    b"\x29\x89"  #   Usage Maximum (\x89)
-    b"\x15\x00"  #   Logical Minimum (0)
-    b"\x25\x89"  #   Logical Maximum (137)
-    b"\x75\x08"  #   Report Size (8)
-    b"\x95\x06"  #   Report Count (6)
-    b"\x81\x00"  #   Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    b"\x05\x08"  #   Usage Page (LEDs)
-    b"\x19\x01"  #   Usage Minimum (Num Lock)
-    b"\x29\x05"  #   Usage Maximum (Kana)
-    b"\x15\x00"  #   Logical Minimum (0)
-    b"\x25\x01"  #   Logical Maximum (1)
-    b"\x75\x01"  #   Report Size (1)
-    b"\x95\x05"  #   Report Count (5)
-    b"\x91\x02"  #   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    b"\x95\x03"  #   Report Count (3)
-    b"\x91\x01"  #   Output (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    b"\xC0"  # End Collection
-*/
-
-uint8_t report_descriptor[] = {
-    0x05, 0x01, // USAGE_PAGE (Generic Desktop)
-    0x09, 0x06, // USAGE (Keyboard)
-    0xa1, 0x01, // COLLECTION (Application)
-    0x85, 0x01, //   REPORT_ID (1)
-    0x05, 0x07, //   USAGE_PAGE (Keyboard)
-    0x19, 0x01, //   USAGE_MINIMUM
-    0x29, 0x7f, //   USAGE_MAXIMUM
-    0x15, 0x00, //   LOGICAL_MINIMUM (0)
-    0x25, 0x01, //   LOGICAL_MAXIMUM (1)
-    0x75, 0x01, //   REPORT_SIZE (1)
-    0x95, 0x08, //   REPORT_COUNT (8)
-    0x81, 0x02, //   INPUT (Data,Var,Abs)
-    0x95, 0x01, //   REPORT_COUNT (1)
-    0x75, 0x08, //   REPORT_SIZE (8)
-    0x81, 0x01, //   INPUT (Cnst,Ary,Abs)
-    0x95, 0x05, //   REPORT_COUNT (5)
-    0x75, 0x01, //   REPORT_SIZE (1)
-    0x05, 0x08, //   USAGE_PAGE (LEDs)
-    0x19, 0x01, //   USAGE_MINIMUM (Num Lock)
-    0x29, 0x05, //   USAGE_MAXIMUM (Kana)
-    0x91, 0x02, //   OUTPUT (Data,Var,Abs)
-    0x95, 0x01, //   REPORT_COUNT (1)
-    0x75, 0x03, //   REPORT_SIZE (3)
-    0x91, 0x01, //   OUTPUT (Cnst,Ary,Abs)
-    0x95, 0x06, //   REPORT_COUNT (6)
-    0x75, 0x08, //   REPORT_SIZE (8)
-    0x15, 0x00, //   LOGICAL_MINIMUM (0)
-    0x25, 0x65, //   LOGICAL_MAXIMUM (101)
-    0x05, 0x07, //   USAGE_PAGE (Keyboard)
-    0x19, 0x00, //   USAGE_MINIMUM (Reserved (no event indicated))
-    0x29, 0x65, //   USAGE_MAXIMUM (Keyboard Application)
-    0x81, 0x00, //   INPUT (Data,Ary,Abs)
-    0xc0        // END_COLLECTION
-};
+#define MODIFIERS_LEFT_CTRL 0x01
+#define MODIFIERS_LEFT_SHIFT 0x02
+#define MODIFIERS_LEFT_ALT 0x04
+#define MODIFIERS_LEFT_GUI 0x08
+#define MODIFIERS_RIGHT_CTRL 0x10
+#define MODIFIERS_RIGHT_SHIFT 0x20
+#define MODIFIERS_RIGHT_ALT 0x40
+#define MODIFIERS_RIGHT_GUI 0x80
 
 typedef struct keyboard_report_t
 {
-  uint8_t modifiers[2];
-  uint8_t key_codes[6];
+  uint8_t modifiersKeys = 0;
+  uint8_t reserved = 0;
+  uint8_t key_codes[6] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
 } keyboard_report_t;
 keyboard_report_t kbd_report;
 
-
 bool deviceConnected = false;
 
-/**  None of these are required as they will be handled by the library with defaults. **
- **                       Remove as you see fit for your needs                        */
 class MyServerCallbacks : public BLEServerCallbacks
 {
   void onConnect(BLEServer *pServer)
   {
+    SerialUSB.println("BLE - OnConnect");
     deviceConnected = true;
   };
 
   void onDisconnect(BLEServer *pServer)
   {
+    SerialUSB.println("BLE - OnDisconnect");
     deviceConnected = false;
   }
-  /***************** New - Security handled here ********************
-  ****** Note: these are the same return values as defaults ********/
+
   uint32_t onPassKeyRequest()
   {
     SerialUSB.println("Server PassKeyRequest");
@@ -253,11 +193,22 @@ class MyServerCallbacks : public BLEServerCallbacks
   {
     SerialUSB.println("Starting BLE work!");
   }
-  /*******************************************************************/
 };
 
-NimBLECharacteristic *reportCharacteristic;
+class OutputCallbacks : public NimBLECharacteristicCallbacks
+{
+  void onWrite(NimBLECharacteristic *pCharacteristic)
+  {
+    SerialUSB.print("Output written. Value = ");
+    SerialUSB.println(pCharacteristic->getValue().c_str());
+  }
+};
+
+NimBLECharacteristic *inputReport;
+NimBLECharacteristic *outputReport;
 NimBLECharacteristic *testReportCharacteristic;
+
+NimBLEAdvertising *pAdvertising;
 
 #define TEST_SERVICE_UUID "6a15b3c8-10e1-11ed-861d-0242ac120002"
 #define TEST_UUID "6a15b3cc-10e1-11ed-861d-0242ac120002"
@@ -278,17 +229,23 @@ void setup()
 
   USB.begin();
 
-  while(!SerialUSB) {}
+  while (!SerialUSB)
+  {
+  }
   SerialUSB.println("BEGIN");
 
   NimBLEDevice::init("ESP32-S3 HID");
-  NimBLEDevice::setPower(ESP_PWR_LVL_P9); /** +9db */
+  NimBLEDevice::setPower(ESP_PWR_LVL_P9);
 
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
   SerialUSB.println("DEVICE INFO SERVICE");
 
+  /*
+  bleKeyboardInstance->hid->pnp(0x02, 0xe502, 0xa111, 0x0210);
+  bleKeyboardInstance->hid->hidInfo(0x00,0x01);
+  */
   BLEService *pDeviceInfoService = pServer->createService(DEVICE_INFO_SERVICE_UUID);
   pDeviceInfoService->createCharacteristic(MODEL_NUMER_UUID, NIMBLE_PROPERTY::READ)->setValue(MODEL_NUMER);
   pDeviceInfoService->createCharacteristic(SERIAL_NUMBER_UUID, NIMBLE_PROPERTY::READ)->setValue(SERIAL_NUMBER);
@@ -296,7 +253,8 @@ void setup()
   pDeviceInfoService->createCharacteristic(HW_REVISION_UUID, NIMBLE_PROPERTY::READ)->setValue(HW_REVISION);
   pDeviceInfoService->createCharacteristic(SW_REVISION_UUID, NIMBLE_PROPERTY::READ)->setValue(SW_REVISION);
   pDeviceInfoService->createCharacteristic(MANUFACTURER_UUID, NIMBLE_PROPERTY::READ)->setValue(MANUFACTURER);
-  pDeviceInfoService->createCharacteristic(PNP_UUID, NIMBLE_PROPERTY::READ)->setValue(pnp);
+  pDeviceInfoService->createCharacteristic(PNP_UUID, NIMBLE_PROPERTY::READ)->setValue({0x02, 0xe502, 0xa111, 0x0210});
+
   pDeviceInfoService->start();
 
   SerialUSB.println("BATTERY SERVICE");
@@ -311,14 +269,19 @@ void setup()
   pHIDService->createCharacteristic(HID_BOOT_INPUT_UUID, NIMBLE_PROPERTY::NOTIFY | NIMBLE_PROPERTY::READ, 8);
   pHIDService->createCharacteristic(HID_BOOT_OUTPUT_UUID, NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::WRITE_NR, 1);
   pHIDService->createCharacteristic(HID_PROTOCOL_MODE_UUID, NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE_NR)->setValue(PROTOCOL_MODE_REPORT);
+  pHIDService->createCharacteristic(HID_INFORMATION_UUID, NIMBLE_PROPERTY::READ, 4)->setValue({ 0x11, 0x1, 0x00, 0x01 });
 
-  // bcdHID (version), bCountryCode (0 not localized), Flags: RemoteWake, NormallyConnectable
-  pHIDService->createCharacteristic(HID_INFORMATION_UUID, NIMBLE_PROPERTY::READ, 4)->setValue({1, 1, 0, 2});
-  pHIDService->createCharacteristic(HID_REPORT_MAP_UUID, NIMBLE_PROPERTY::READ, sizeof(report_descriptor))->setValue(report_descriptor);
+  pHIDService->createCharacteristic(HID_REPORT_MAP_UUID, NIMBLE_PROPERTY::READ, sizeof(HIDReportDescriptor))->setValue(HIDReportDescriptor);
   pHIDService->createCharacteristic(HID_SUSPENDED_UUID, NIMBLE_PROPERTY::WRITE_NR, 1)->setValue(1);
-  
-  reportCharacteristic = pHIDService->createCharacteristic(HID_REPORT_UUID, NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::NOTIFY, sizeof(kbd_report));
-  reportCharacteristic->createDescriptor(HID_REPORT_DESCRIPTOR_UUID)->setValue("<BB01");
+
+  uint8_t inDescriptor[] = {REPORT_KEYBOARD_ID, 0x01};
+  inputReport = pHIDService->createCharacteristic((uint16_t)0x2a4d, NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::NOTIFY | NIMBLE_PROPERTY::READ_ENC);
+  inputReport->createDescriptor((uint16_t)0x2908, NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::READ_ENC)->setValue(inDescriptor, 2);
+
+  uint8_t outDescriptor[] = {REPORT_KEYBOARD_ID, 0x02};
+  outputReport = pHIDService->createCharacteristic((uint16_t)0x2a4d, NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::WRITE_NR | NIMBLE_PROPERTY::READ_ENC | NIMBLE_PROPERTY::WRITE_ENC);
+  outputReport->createDescriptor((uint16_t)0x2908, NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::READ_ENC | NIMBLE_PROPERTY::WRITE_ENC)->setValue(outDescriptor, 2);
+  outputReport->setCallbacks(new OutputCallbacks());
 
   pHIDService->start();
 
@@ -333,7 +296,7 @@ void setup()
 
   SerialUSB.println("ADVERTISING");
 
-  NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
+  pAdvertising = NimBLEDevice::getAdvertising();
   pAdvertising->setName("ESP32-S3 HID");
   pAdvertising->addServiceUUID("ABCD");
   pAdvertising->setAppearance(APPEARANCE_KEYBOARD);
@@ -342,13 +305,12 @@ void setup()
 
 void SendKeyReport(uint8_t key)
 {
-  kbd_report.modifiers[0] = 0;
-  kbd_report.modifiers[1] = 0;
+  kbd_report.modifiersKeys = 0;
 
   kbd_report.key_codes[0] = key;
 
-  reportCharacteristic->setValue(kbd_report);
-  reportCharacteristic->notify();
+  inputReport->setValue(kbd_report);
+  inputReport->notify();
 
   testReportCharacteristic->setValue(kbd_report);
   testReportCharacteristic->notify();
@@ -356,36 +318,182 @@ void SendKeyReport(uint8_t key)
 
 void ReleaseAll()
 {
-  kbd_report.modifiers[0] = 0;
-  kbd_report.modifiers[1] = 0;
+  kbd_report.modifiersKeys = 0;
 
-  for(int x = 0; x < 6; x++)
+  for (int x = 0; x < 6; x++)
   {
     kbd_report.key_codes[x] = 0;
   }
 
-  reportCharacteristic->setValue(kbd_report);
-  reportCharacteristic->notify();
+  inputReport->setValue(kbd_report);
+  inputReport->notify();
 
   testReportCharacteristic->setValue(kbd_report);
   testReportCharacteristic->notify();
 }
 
+bool oldConnected = false;
 void loop()
 {
-
-  if(deviceConnected)
+  if (deviceConnected)
   {
+    if(!oldConnected)
+    {
+      delay(5000);
+
+      oldConnected = true;
+      pAdvertising->stop();
+    }
+
+    if(!deviceConnected)
+    {
+      return;
+    }
+
     SerialUSB.println("Sending Z!");
 
     SendKeyReport(KEY_Z);
-    delay(3000);
+    delay(10);
     ReleaseAll();
-    delay(3000);
+
+    delay(1000);
   }
   else
   {
+    if(oldConnected)
+    {
+      oldConnected = false;
+      pAdvertising->start();
+    }
+
     SerialUSB.println("Lap!");
     delay(500);
   }
 }
+
+/*
+#include <BleKeyboard.h>
+
+static void usbEventCallback(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+
+  if (event_base == ARDUINO_USB_EVENTS)
+  {
+    arduino_usb_event_data_t *data = (arduino_usb_event_data_t *)event_data;
+    switch (event_id)
+    {
+    case ARDUINO_USB_STARTED_EVENT:
+      // HWSerial.println("USB PLUGGED");
+      break;
+    case ARDUINO_USB_STOPPED_EVENT:
+      // HWSerial.println("USB UNPLUGGED");
+      break;
+    case ARDUINO_USB_SUSPEND_EVENT:
+      // HWSerial.printf("USB SUSPENDED: remote_wakeup_en: %u\n", data->suspend.remote_wakeup_en);
+      break;
+    case ARDUINO_USB_RESUME_EVENT:
+      // HWSerial.println("USB RESUMED");
+      break;
+
+    default:
+      break;
+    }
+  }
+  else if (event_base == ARDUINO_USB_CDC_EVENTS)
+  {
+    arduino_usb_cdc_event_data_t *data = (arduino_usb_cdc_event_data_t *)event_data;
+    switch (event_id)
+    {
+    case ARDUINO_USB_CDC_CONNECTED_EVENT:
+      // HWSerial.println("CDC CONNECTED");
+      break;
+    case ARDUINO_USB_CDC_DISCONNECTED_EVENT:
+      // HWSerial.println("CDC DISCONNECTED");
+      break;
+    case ARDUINO_USB_CDC_LINE_STATE_EVENT:
+      // HWSerial.printf("CDC LINE STATE: dtr: %u, rts: %u\n", data->line_state.dtr, data->line_state.rts);
+      break;
+    case ARDUINO_USB_CDC_LINE_CODING_EVENT:
+      // HWSerial.printf("CDC LINE CODING: bit_rate: %u, data_bits: %u, stop_bits: %u, parity: %u\n", data->line_coding.bit_rate, data->line_coding.data_bits, data->line_coding.stop_bits, data->line_coding.parity);
+      break;
+    case ARDUINO_USB_CDC_RX_EVENT:
+      // HWSerial.printf("CDC RX [%u]:", data->rx.len);
+      //{
+      //     uint8_t buf[data->rx.len];
+      //     size_t len = USBSerial.read(buf, data->rx.len);
+      //     HWSerial.write(buf, len);
+      // }
+      // HWSerial.println();
+      break;
+    case ARDUINO_USB_CDC_RX_OVERFLOW_EVENT:
+      // HWSerial.printf("CDC RX Overflow of %d bytes", data->rx_overflow.dropped_bytes);
+      break;
+
+    default:
+      break;
+    }
+  }
+}
+
+USBCDC SerialUSB;
+//USBHIDKeyboard Keyboard;
+
+BleKeyboard bleKeyboard;
+
+void setup()
+{
+  USB.usbClass(0);
+  USB.usbSubClass(0);
+  USB.usbProtocol(0);
+  USB.manufacturerName("PAROZZZ");
+  USB.productName("PAROZZZ_USB");
+
+  USB.onEvent(usbEventCallback);
+  SerialUSB.onEvent(usbEventCallback);
+
+  SerialUSB.begin(115200);
+  //Keyboard.begin();
+
+  USB.begin();
+
+  while (!SerialUSB)
+  {
+  }
+
+  SerialUSB.println("BEGIN");
+
+  SerialUSB.println("Starting BLE work!");
+  bleKeyboard.begin();
+}
+
+void loop()
+{
+  if (bleKeyboard.isConnected())
+  {
+    SerialUSB.println("Sending 'Hello world'...");
+    bleKeyboard.print("Hello world");
+
+    delay(1000);
+
+    SerialUSB.println("Sending Enter key...");
+    bleKeyboard.write(KEY_RETURN);
+
+    delay(1000);
+
+    //SerialUSB.println("Sending Play/Pause media key...");
+    //bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
+
+    //delay(1000);
+
+    //SerialUSB.println("Sending Ctrl+Alt+Delete...");
+    //bleKeyboard.press(KEY_LEFT_CTRL);
+    //bleKeyboard.press(KEY_LEFT_ALT);
+    //bleKeyboard.press(KEY_DELETE);
+    delay(100);
+    bleKeyboard.releaseAll();
+  }
+
+  SerialUSB.println("Waiting 5 seconds...");
+  delay(5000);
+}
+*/
